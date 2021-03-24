3b347c417d90ddec8d2e70776714d52f7979da3a
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-03-23 22:16:48 -0700

    Practicing with range minimum queries on static arrays.

--- rangequeries/queries.py
+++ rangequeries/queries.py
@@ -16,16 +16,36 @@ class Query:
             else:
                 self.prefix_sum_array.append(el + self.prefix_sum_array[ind - 1])
 
-    def sum_query(self, range_start, range_end):
+    def range_sum(self, range_start, range_end):
         return self.prefix_sum_array[range_end] - self.prefix_sum_array[range_start - 1]
+
+    def range_minimum(self, range_start, range_end):
+        if not range_end - range_start:
+            return self.array[range_start]
+        else:
+            length = range_end - range_start + 1
+            if (length != 0 ) and (length & (length - 1) == 0):
+                length = int(length / 2)
+                return min(self.range_minimum(range_start, range_start + length - 1), self.range_minimum(range_start + length, range_end))
+            else:
+                closest_power_two = 2
+                while closest_power_two**2 <= length:
+                    closest_power_two = closest_power_two**2
+                return min(self.range_minimum(range_start, range_start + closest_power_two - 1), self.range_minimum(range_end - closest_power_two + 1, range_end))
 
 def main():
     query = Query([1, 3, 4, 8, 6, 1, 4, 2])
+    print("This is the array being worked on:")
     query.show_array()
-    query.generate_prefix_sum_array()
-    query.show_prefix_sum_array()
 
-    print(query.sum_query(3, 6))
+    query.generate_prefix_sum_array()
+    print("This is its prefix array sum:")
+    query.show_prefix_sum_array()
+    print("This is the sum of the elements from index 3 to index 6 of the array:")
+    print(query.range_sum(3, 6))
+    print("This is the minimum of the elements from index 1 to index 6 of the array:")
+    print(query.range_minimum(1, 6))
+
 
 
 if __name__ == "__main__":
